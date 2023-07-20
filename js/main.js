c5016dc3d6f746ed66c368b3d343e7e2ab088644
Front-Ace <mahmoudarabia2019@gmail.com>
2023-07-20 22:24:50 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var  firstInput = document.getElementById("site-name");
 var secondInput = document.getElementById("site-url");
 var allSites = [];
+var usedId = null;
 if (localStorage.getItem("allSites") != null) {
         allSites = JSON.parse(localStorage.getItem("allSites"));
         displayAllSites();
@@ -38,8 +39,10 @@ function displayAllSites() {
         var cartona= "";
         for(i=0 ; i< allSites.length ; i++){
                 cartona += `<div class="reading row py-2 d-flex align-items-center">
-                <div class="col-3"> ${i} </div>
-                <div class="col-3"> ${allSites[i].name} </div>
+                <div class="col-1"> ${i} </div>
+                <div class="col-2"> ${allSites[i].name} </div>
+                <div class="col-3"><button onclick="updateLink( ${i} )" id="update" class="btn btn-primary px-3"><i class="fa-solid fa-pen-to-square me-1"></i>
+                        Update</button></div>
                 <div class="col-3 "><button onclick="visitLink( ${i} )" id="visit" class="btn btn-success px-3"><i class="fa-solid fa-eye me-1"></i>
                         Visit</button></div>
                 <div class="col-3 "><button onclick="deleteElement( ${i} )" id="delete" class="btn btn-danger "><i class="fa-solid fa-trash-can me-1"></i>
@@ -58,10 +61,35 @@ function deleteElement(idx) {
 function visitLink(idxlink) {
         open(allSites[idxlink].link)
 }
+function updateLink(idx) {
+        usedId = idx;
+        firstInput.value = allSites[idx].name;
+        secondInput.value = allSites[idx].link;
+        document.getElementById("submit").innerHTML = `<button onclick="updateWhole( )"class="btn btn-primary px-5 py-2">Update</button>`
+}
 
 function validateURL() {
         var regex = /^https:\/\/.*\.com$/;
         return regex.test(secondInput.value);
+}
+function updateWhole() {
+        var newSite = {
+                name: firstInput.value ,
+                link: secondInput.value 
+            };
+            if (usedId !== null) {
+                allSites.splice(usedId,1,newSite);
+                console.log(`id: ${usedId} is updated name ${newSite.name} link ${newSite.link}`);
+                usedId = null;
+            }
+
+       
+        console.log(allSites);
+        displayAllSites();
+        firstInput.value= "";
+        secondInput.value= "";
+        document.getElementById("submit").innerHTML = `<button onclick="addNewSite()"class="btn btn-danger px-5 py-2">Submit</button>`
+        localStorage.setItem("allSites", JSON.stringify(allSites))
 }
 
 
